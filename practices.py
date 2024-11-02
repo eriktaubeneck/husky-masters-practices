30803e6e431da81ba0fb5626ae16dc8a072de3ba
Erik Taubeneck <erik.taubeneck@gmail.com>
2024-11-02 15:58:19 -0700

    first commit

---
+++ practices.py
@@ -0,0 +1,88 @@
+import calendar
+from datetime import datetime, time, timedelta
+from enum import Enum
+
+cal = calendar.Calendar()
+
+
+# Define the Weekday Enum
+class Weekday(Enum):
+    MONDAY = 0
+    TUESDAY = 1
+    WEDNESDAY = 2
+    THURSDAY = 3
+    FRIDAY = 4
+    SATURDAY = 5
+    SUNDAY = 6
+
+
+coaches = {
+    "addie": "Addie P.",
+    "alex": "Alex P.",
+    "erik": "Erik T.",
+    "matt": "Matt J.",
+    "nate": "Nate K",
+}
+
+# Set the target weekday and initialize DEFAULT_START_DATE
+practice_days = [Weekday.TUESDAY, Weekday.THURSDAY]
+today = datetime.today()
+
+# Move to the first day of the next month
+next_month = datetime.combine(
+    (today.replace(day=1) + timedelta(days=32)).replace(day=1),
+    time(0, 0)
+)
+
+month_and_year = input(f"MM/YY? (Default {next_month.strftime('%m/%y')}): ")
+if month_and_year == "":
+    month, year = next_month.month, next_month.year
+else:
+    month, year = int(month_and_year.split("/")[0]), int(month_and_year.split("/")[1])
+
+practice_dates = (
+    d for d in
+    cal.itermonthdates(year, month)
+    if d.weekday() in [pd.value for pd in practice_days]
+    and d.month == next_month.month
+)
+
+
+CSV_HEADER = (
+    "EVENT NAME,EVENT EXCERPT,EVENT VENUE NAME,EVENT ORGANIZER NAME,EVENT START DATE,"
+    "EVENT START TIME,EVENT END DATE,EVENT END TIME,ALL DAY EVENT,TIMEZONE,"
+    "HIDE FROM EVENT LISTINGS,STICKY IN MONTH VIEW,EVENT CATEGORY,EVENT TAGS,"
+    "EVENT COST,EVENT CURRENCY SYMBOL,EVENT CURRENCY POSITION,EVENT ISO CURRENCY CODE,"
+    "EVENT FEATURED IMAGE,EVENT WEBSITE,EVENT SHOW MAP LINK,EVENT SHOW MAP,"
+    "ALLOW COMMENTS,ALLOW TRACKBACKS AND PINGBACKS,EVENT DESCRIPTION\n"
+)
+
+CSV_ROW_TEMPLATE = (
+    "{practice_title},,View Ridge Swim & Tennis Club,{coach},{start_dt},{end_dt},FALSE,America/Los_Angeles,"
+    "FALSE,FALSE,Practice,,,,,,,,,FALSE,FALSE\n"
+)
+
+filename = f"{next_month.strftime('%Y-%m-%d')}-practices.csv"
+
+with open(filename, "w+") as f:
+    f.write(CSV_HEADER)
+    for practice_date in practice_dates:
+        practice_title = input(
+            f"provide practice title (or \"skip\") for "
+            f"{practice_date.strftime('%a %m-%d')}: "
+        )
+        if practice_title != "skip":
+            coach_str = input("who's the coach? ")
+            while coach_str not in coaches.keys():
+                coach_str = input(
+                    f"invalid selection. pick from {', '.join(coaches.keys())}: "
+                )
+
+            start_dt = datetime.combine(practice_date, time(20, 20))
+            end_dt = datetime.combine(practice_date, time(21, 20))
+            f.write(CSV_ROW_TEMPLATE.format(
+                practice_title=practice_title,
+                start_dt=start_dt.strftime("%Y/%m/%d,%-I:%M%p"),
+                end_dt=end_dt.strftime("%Y/%m/%d,%-I:%M%p"),
+                coach=coaches[coach_str],
+            ))
---
+++ tickets.py
@@ -0,0 +1,70 @@
+import argparse
+import csv
+import sys
+from datetime import datetime
+
+# Set up argument parser
+parser = argparse.ArgumentParser(description="Extract event start dates from CSV file")
+parser.add_argument('filename', type=str, help="The path to the CSV file")
+parser.add_argument('starting_id', type=int, help="The ID of the first practice")
+
+# Parse the arguments
+args = parser.parse_args()
+
+# Check if filename was provided
+if not args.filename:
+    print("Error: CSV filename is required as a command-line argument.")
+    sys.exit(1)
+
+# Check if starting_id was provided
+if not args.starting_id:
+    print("Error: starting_id is required as a command-line argument.")
+    sys.exit(1)
+
+
+# Open the CSV file specified by the command-line argument
+with open(args.filename, 'r') as csvfile:
+    reader = csv.DictReader(csvfile)
+
+    # Extract the start dates
+
+    practice_dates = [
+        datetime.strptime(row['EVENT START DATE'], "%Y/%m/%d") for row in reader
+    ]
+
+
+tickets_filename = "{0}-tickets.{1}".format(*args.filename.split("."))
+
+CSV_HEADER = (
+    "\"Event Name, ID, or Slug\",Ticket Name,Ticket Description,Ticket Start Sale Date,"
+    "Ticket Start Sale Time,Ticket End Sale Date,Ticket End Sale Time,Ticket Stock, "
+    "Ticket Price\n"
+)
+
+CSV_ROW = (
+    "{_id},{ticket_name},,{start_date},{start_time},{end_date},{end_time},{ticket_count},"
+    "{price}\n"
+)
+
+ticket_count = 40
+price = 15
+start_date = datetime.today().strftime("%Y/%m/%d")
+start_time = "12:01 AM"
+end_time = "11:59 PM"
+
+with open(tickets_filename, 'w+') as f:
+    f.write(CSV_HEADER)
+    for i, practice in enumerate(practice_dates):
+        _id = args.starting_id + i
+        ticket_name = f"practice{practice.strftime('%Y%m%d')}"
+        end_date = practice.strftime("%Y/%m/%d")
+        f.write(CSV_ROW.format(
+            _id=_id,
+            ticket_name=ticket_name,
+            start_date=start_date,
+            start_time=start_time,
+            end_date=end_date,
+            end_time=end_time,
+            ticket_count=ticket_count,
+            price=price
+        ))
